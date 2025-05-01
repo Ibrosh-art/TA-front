fe895108e8077852e51741386ba14049337d2f3a
adilhan <adilhansatymkulov40@gmail.com>
2025-05-01 17:24:00 +0600

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,11 +14,11 @@ const Hero = (props) => {
         
         <div className="size-full">
           <div className="mt-24 px-5 sm:px-10">
-            <h1 className="h1_hero special-font hero-heading text-blue-100">
+            <h1 className="h1_hero special-font hero-heading text-blue-100 drop-shadow-md">
               DORD<b></b>OI
             </h1>
 
-            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+            <p className="mb-5 max-w-64 font-robert-regular text-blue-100 drop-shadow-md">
               Discover the future of the world <br /> With Dordoi association
             </p>
 
@@ -37,7 +37,7 @@ const Hero = (props) => {
         <video autoPlay loop muted  className=" w-[50vw] object-cover object-center rounded-3xl">
           <source src={props.src} type="video/mp4"/>
         </video>
-        <h1 className="special-font hero-heading absolute bottom-5">
+        <h1 className="special-font hero-heading absolute bottom-5 drop-shadow-md">
         FUTURE
         </h1>
 
@@ -46,4 +46,4 @@ const Hero = (props) => {
   );
 };
 
-export default Hero;
+export default React.memo(Hero);
